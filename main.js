6988f7f93973d24762a2d3b8269832318fa8ce45
ArdaGurcan <ardagurcan2004@gmail.com>
2022-06-09 17:27:42 +0300

    minor improvements

--- main.js
+++ main.js
@@ -1,75 +1,66 @@
 var rays = [];
 var walls = [];
-const n = 36;
+const n = 200;
 
 function setup() {
-    createCanvas(800, 800)
+    var cnv = createCanvas(windowWidth, windowHeight);
+    cnv.style("display", "block");
     for (let i = 0; i < 360; i += 360 / n) {
-        rays.push(new Ray(createVector(400, 400), radians(i)))
-
+        rays.push(new Ray(createVector(400, 400), radians(i)));
     }
-    for (let i = 0; i < 5; i++) {
-        walls.push(new Boundary(random(0, width), random(0, width), random(0, height), random(0, height)));
-    }
-    walls.push(new Boundary(0, 0, width, 0))
-    walls.push(new Boundary(0, 0, 0, height))
-    walls.push(new Boundary(width, 0, width, height))
-    walls.push(new Boundary(0, height, width, height))
+    for (let i = 0; i < 15; i++) {
+        let x1 = random(100, width-100);
+        let x2 = x1 + random(50, 100);
+        let y1 = random(100, height-100);
+        let y2 = y1 + random(50, 100);
 
+        walls.push(new Boundary(x1, y1, x2, y1));
+        walls.push(new Boundary(x2, y1, x2, y2));
+        walls.push(new Boundary(x2, y2, x1, y2));
+        walls.push(new Boundary(x1, y2, x1, y1));
+    }
+
+    walls.push(new Boundary(0, 0, width, 0));
+    walls.push(new Boundary(0, 0, 0, height));
+    walls.push(new Boundary(width, 0, width, height));
+    walls.push(new Boundary(0, height, width, height));
 }
 
 function draw() {
-    background(50, 50, 60)
-
+    background(50, 50, 60);
 
     for (let r = 0; r < rays.length; r++) {
         if (mouseX) {
-            rays[r].pos = createVector(mouseX, mouseY)
-
+            rays[r].pos = createVector(mouseX, mouseY);
         }
         let record = Infinity;
         let closest = null;
-        //rays[r].setAngle(radians(r))
 
-        // rays[r].show()
         for (let i = 0; i < walls.length; i++) {
-            walls[i].show()
+            walls[i].show();
 
-            let pt = null
+            let pt = null;
 
-            pt = rays[r].intersect(walls[i])
+            pt = rays[r].intersect(walls[i]);
             if (pt) {
-                d = (rays[r].pos.x - pt.x) ** 2 + (rays[r].pos.y - pt.y) ** 2
-                // d = dist(rays[r].pos.x, rays[r].pos.y, pt.x, pt.y)
+                d = (rays[r].pos.x - pt.x) ** 2 + (rays[r].pos.y - pt.y) ** 2;
                 if (d < record) {
                     record = d;
                     closest = pt;
                 }
-
             }
         }
         if (closest) {
-            pt = closest
-
+            pt = closest;
         }
         if (pt) {
-            strokeWeight(2)
+            strokeWeight(2);
             stroke(230, 217, 69, 90);
             line(rays[r].pos.x, rays[r].pos.y, pt.x, pt.y);
-
         }
-
-
     }
-
-
-
-
-
-
-
 }
 
-function mousePressed() {
-
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
 }
